f42ab56c3af07eb2d240d7b254ec3df504d126be
dragon28 <sengloong82@gmail.com>
2024-02-16 16:17:04 +0800

    Add files via upload

--- playwright_async.py
+++ playwright_async.py
@@ -1,28 +1,79 @@
 import asyncio
 import time
-from playwright.async_api import async_playwright
+from playwright.async_api import Playwright, async_playwright
+from playwright.async_api import Page, expect
+import csv
 
-async def task_playwright():
+async def scrape_data(page):
     
-    async with async_playwright() as async_playwright:
-        
-        browser = await async_playwright.chromium.launch()
-        
-        page = await browser.new_page()
-        
-        await page.goto("https://scrapeme.live/shop/", timeout=0)
-        
-        time.sleep(3)
-        
-        print(await page.title())
-        
-        await browser.close()
+    scraped_elements = []
+ 
+    items = await page.query_selector_all("li.product")
+ 
+    for i in items:
+     
+        scraped_element = {}
+
+        el_title = await i.query_selector("h2")
+
+        scraped_element["product"] = await el_title.inner_text()
+
+        el_price = await i.query_selector("span.woocommerce-Price-amount")
+
+        scraped_element["price"] = await el_price.text_content()
+
+        image = await i.query_selector("a.woocommerce-LoopProduct-link.woocommerce-loop-product__link > img")
+
+        scraped_element["img_link"] = await image.get_attribute("src")
+
+        scraped_elements.append(scraped_element)
+    
+    return scraped_elements
 
 
-async def main():
+async def save_as_csv(data):
     
-    await task_playwright()
+	with open("async_scraped_data.csv", "w", newline="") as csvfile:
+     
+		fields = ["product", "price", "img_link"]
+  
+		writer = csv.DictWriter(csvfile, fieldnames=fields, quoting=csv.QUOTE_ALL)
+  
+		writer.writeheader()
+  
+		writer.writerows(data)
+
+
+async def run(playwright: Playwright) -> None:
     
+    browser = await playwright.chromium.launch(headless=False)
+ 
+    context = await browser.new_context()
+
+    page = await context.new_page()
+
+    await page.goto("https://scrapeme.live/shop/", timeout=0)
+    
+    time.sleep(3)
+    
+    data = await scrape_data(page)
+
+    print(data)
+    
+    await save_as_csv(data)
+
+    await context.close()
+    
+    await browser.close()
+
+
+async def main() -> None:
+    
+    async with async_playwright() as asyncplaywright:
+        
+        await run(asyncplaywright)
+
+
 if __name__ == "__main__":
     
     asyncio.run(main())
--- playwright_sync.py
+++ playwright_sync.py
@@ -1,28 +1,78 @@
 import time
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import Playwright, sync_playwright
 from playwright.sync_api import Page, expect
+import csv
 
-def task_playwright():
+def scrape_data(page):
     
-    with sync_playwright() as sync_playwright:
-        
-        browser = sync_playwright.chromium.launch()
-        
-        page = browser.new_page()
-        
-        page.goto("https://scrapeme.live/shop/", timeout=0)
-        
-        time.sleep(3)
-        
-        print(page.title())
-        
-        browser.close()
+    scraped_elements = []
+ 
+    items = page.query_selector_all("li.product")
+ 
+    for i in items:
+     
+        scraped_element = {}
+
+        el_title = i.query_selector("h2")
+
+        scraped_element["product"] = el_title.inner_text()
+
+        el_price = i.query_selector("span.woocommerce-Price-amount")
+
+        scraped_element["price"] = el_price.text_content()
+
+        image = i.query_selector("a.woocommerce-LoopProduct-link.woocommerce-loop-product__link > img")
+
+        scraped_element["img_link"] = image.get_attribute("src")
+
+        scraped_elements.append(scraped_element)
+    
+    return scraped_elements
+
+
+def save_as_csv(data):
+    
+	with open("sync_scraped_data.csv", "w", newline="") as csvfile:
+     
+		fields = ["product", "price", "img_link"]
+  
+		writer = csv.DictWriter(csvfile, fieldnames=fields, quoting=csv.QUOTE_ALL)
+  
+		writer.writeheader()
+  
+		writer.writerows(data)
+
+
+def run(playwright: Playwright) -> None:
+    
+    browser = playwright.chromium.launch(headless=False)
+ 
+    context = browser.new_context()
+
+    page = context.new_page()
+
+    page.goto("https://scrapeme.live/shop/", timeout=0)
+    
+    time.sleep(3)
+    
+    data = scrape_data(page)
+
+    print(data)
+    
+    save_as_csv(data)
+
+    context.close()
+    
+    browser.close()
 
 
 def main():
     
-    task_playwright()
+    with sync_playwright() as syncplaywright:
+        
+        run(syncplaywright)
     
 
 if __name__ == "__main__":
+    
     main()
